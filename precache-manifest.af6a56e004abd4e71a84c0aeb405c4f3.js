842c15ab5c78876527b2e6bdcda72111e5c238be
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-01 23:46:49 +0000

    Deploy pythonsul/pythonsul2019-site to github.com/pythonsul/pythonsul2019-site.git:gh-pages

---
+++ precache-manifest.af6a56e004abd4e71a84c0aeb405c4f3.js
@@ -0,0 +1,145 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "/./assets/0600b980e0a3269432be92707c706da5.png"
+  },
+  {
+    "url": "/./assets/072de0c8eba15776de5dde36e53f8199.png"
+  },
+  {
+    "url": "/./assets/0b1d3016cc9e02765a753465cbb8da1b.svg"
+  },
+  {
+    "url": "/./assets/1040749f9476348718b8f6e0e20dc921.jpg"
+  },
+  {
+    "url": "/./assets/16501d09af955cfe8963e614bbdfd443.svg"
+  },
+  {
+    "url": "/./assets/19fd2f706aa097db8fadf629de04ac12.svg"
+  },
+  {
+    "url": "/./assets/2d8c4ac9b6be2e2661847ccbaf5abf01.svg"
+  },
+  {
+    "url": "/./assets/4429bbb1fadd88660033c60e25b0d6d0.svg"
+  },
+  {
+    "url": "/./assets/46e3f79e1df67592da7547bb7eb74e1d.svg"
+  },
+  {
+    "url": "/./assets/4dfad6db7c9efb2ca4299679ee004ca9.png"
+  },
+  {
+    "url": "/./assets/5cda9de029659fb6f704f6d6f6de986d.svg"
+  },
+  {
+    "url": "/./assets/66c5c1f941cd0dd1f9f3e9425047efd9.svg"
+  },
+  {
+    "url": "/./assets/676242ba1bd587206eb3800c0289d0e0.svg"
+  },
+  {
+    "url": "/./assets/69454a80c352c399644073b2d991d6ae.svg"
+  },
+  {
+    "url": "/./assets/745113de3d06753dafba58ebc50c5dc4.svg"
+  },
+  {
+    "url": "/./assets/74dabe5df1983c6b3404b09b10bdac00.svg"
+  },
+  {
+    "url": "/./assets/87bb797a285f23fb14890d69fb0db2ba.svg"
+  },
+  {
+    "url": "/./assets/8a84d937145140ca758b360009fa410d.svg"
+  },
+  {
+    "url": "/./assets/9f307dcbb8cc2862f7f603d1d9df1787.png"
+  },
+  {
+    "url": "/./assets/a1242e40842339aaa8ee7d810318529c.svg"
+  },
+  {
+    "url": "/./assets/a1cb45781a9958b2542736ff0f094dcd.svg"
+  },
+  {
+    "url": "/./assets/a209d0cd4e6902352a419f276d5dde03.svg"
+  },
+  {
+    "url": "/./assets/ad35fa18a9bb7058faeeedb6d6d2a274.svg"
+  },
+  {
+    "url": "/./assets/bccf71d1f7059651960c22c8acfa48b5.svg"
+  },
+  {
+    "url": "/./assets/c1e6bce66e35f88b2fdd73d713b8598e.svg"
+  },
+  {
+    "url": "/./assets/c5162cd0c480a6aabfd03d8eea76e82e.svg"
+  },
+  {
+    "url": "/./assets/cace3226fc585b35e358235e2e463b77.svg"
+  },
+  {
+    "url": "/./assets/cc3708b4900e1f1331becf63253c9c97.png"
+  },
+  {
+    "url": "/./assets/d63efcfdc56c853dd75c34c995806ed8.svg"
+  },
+  {
+    "url": "/./assets/d8bc8ada804c39bd271c7ad46170b546.svg"
+  },
+  {
+    "url": "/./assets/e39b39fd14903457ea5f15a79c8cdf19.svg"
+  },
+  {
+    "url": "/./assets/fe2203988e4d283e51dc0a80b8041b19.svg"
+  },
+  {
+    "revision": "8e79ac0de212f41ea6d818cbafb74b47",
+    "url": "/./assets/icon144.png"
+  },
+  {
+    "revision": "1b1d17a73e13f56f4a87b2a64ded8aa0",
+    "url": "/./assets/icon168.png"
+  },
+  {
+    "revision": "f7488e5e8a4df180325956bf68df1e93",
+    "url": "/./assets/icon192.png"
+  },
+  {
+    "revision": "6751ee2c4198f0c220f22739b46741b1",
+    "url": "/./assets/icon48.png"
+  },
+  {
+    "revision": "f4c5d509fb792b1ec470deb63726cf78",
+    "url": "/./assets/icon512.png"
+  },
+  {
+    "revision": "174a7b910aa5c6053581c73a1edf1557",
+    "url": "/./assets/icon72.png"
+  },
+  {
+    "revision": "8ec7c5389ee5622d1395187ac172f700",
+    "url": "/./assets/icon96.png"
+  },
+  {
+    "revision": "21ab5d3951f82ab30065391e61d446ee",
+    "url": "/./documents/Plano_patrocinio_PythonBR_2018_V5.pdf"
+  },
+  {
+    "revision": "6f28af5ea8fcf8c0c3e678552076ed5e",
+    "url": "/./documents/PythonBR14-MidiaKit.zip"
+  },
+  {
+    "revision": "a2e35346687e7a10e1ce",
+    "url": "/82d82bc84aeed423327e.css"
+  },
+  {
+    "url": "/main.1370e0829f07ad59e3f2.js"
+  },
+  {
+    "revision": "3b882890c4f9a82bad90790ea475cd19",
+    "url": "/sw.js"
+  }
+]);
